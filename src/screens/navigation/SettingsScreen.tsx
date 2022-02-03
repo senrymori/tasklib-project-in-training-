208cb67b73a0e337fdd672dac18cd93b091de192
renai marsh <senrymori@gmail.com>
2022-02-03 17:38:57 +0600

    setting

--- src/screens/navigation/SettingsScreen.tsx
+++ src/screens/navigation/SettingsScreen.tsx
@@ -1,7 +1,17 @@
 import React from "react"
+import { balls } from "../components/balls"
+import { ArrowBtn, ContainerBtn, NavContainer, NavHeader, NavHeaderTitle } from "../components/ContainerNav"
 
-export const SettingsScreen: React.FC = () => {
+export const SettingsScreen: React.FC = ({navigation}) => {
     return (
-        <></>
+        <NavContainer>
+            {balls.NavBall}
+            <NavHeader>
+                <NavHeaderTitle>Settings</NavHeaderTitle>
+                <ContainerBtn onPress={() => navigation.goBack()}>
+                    <ArrowBtn />
+                </ContainerBtn>
+            </NavHeader>
+        </NavContainer>
     )
 }
